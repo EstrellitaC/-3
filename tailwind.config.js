5fe42bb4a5f2b0272671115f0e916bc8c2eafbf8
EstrellitaC <estrellita.caycedo@tecsup.edu.pe>
2024-09-08 23:56:41 -0500

    holi

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+const plugin = require('tailwindcss/plugin')
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      keyframes: {
+        rotate: {
+          '0%': {
+            transform: 'rotate(0deg)'
+          },
+          '100%': {
+            transform: 'rotate(360deg)'
+          }
+        }
+      },
+      textShadow: {
+        sm: '5px 5px 1px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+      animation: {
+        word: 'word 3s 1',
+        rotate: 'rotate 3s infinite'
+      },
+      transitionProperty: {
+        'name': 'opacity, transform'
+      },
+      transitionDuration: {
+        '500': '0.5s'
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
+}
